4352fcd244d79613be5783aed626d7ac1f3e2d26
Thiago Prates <thipratesdp@gmail.com>
2021-10-01 10:32:54 -0300

    Adding the main files
    Adding the HTML, CSS and JS files.

---
+++ script.js
@@ -0,0 +1,8 @@
+// // Alerta com uma mensagem
+// window.alert("Hello World !");
+
+// // Janela com botoes de confirmacao / cancelar
+// window.confirm("Você aprendeu a emitir mensagens de alerta em JS?");
+
+// // Janela com uma campo de texto para o usuario digitar;
+// window.prompt("Em qual empresa você gostaria de trabalhar ?");
