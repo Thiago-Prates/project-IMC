36568931c2c913951c160012db6562f962b8b316
Thiago Prates <thipratesdp@gmail.com>
2021-10-08 07:25:10 -0300

    updating script.js
    Adding the calculate IMC feature to the project.

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-// // Alerta com uma mensagem
-// window.alert("Hello World !");
+const buttonCalc = document.getElementById('calcular');
 
-// // Janela com botoes de confirmacao / cancelar
-// window.confirm("Você aprendeu a emitir mensagens de alerta em JS?");
+const name = document.getElementById('nome').value;
+const weight = document.getElementById('peso').value;
+const height = document.getElementById('altura').value;
+const result = document.getElementById('resultado');
 
-// // Janela com uma campo de texto para o usuario digitar;
-// window.prompt("Em qual empresa você gostaria de trabalhar ?");
+if (name != '' && weight != '' && height != '') {
+  const imcValue = (weight / height ** 2).toFixed(1);
+
+  let situation = '';
+
+  if (imcValue < 18.5) {
+    situation = 'Abaixo do peso';
+  } else if (imcValue < 25) {
+    situation = 'Peso ideal';
+  } else if (imcValue < 30) {
+    situation = 'Levemente acima do peso';
+  } else if (imcValue < 35) {
+    situation = 'Obesidade nível I';
+  } else if (imcValue < 40) {
+    situation = 'Obesidade nível II';
+  } else {
+    situation = 'Obesidade nível III';
+  }
+}
