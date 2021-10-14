9c728f91ab76c08a5bb70d65a3edfe5e7db36fb5
Thiago Prates <thipratesdp@gmail.com>
2021-10-14 09:46:59 -0300

    Adding new feature

--- script.js
+++ script.js
@@ -5,11 +5,15 @@ function calculateImc() {
   const height = document.getElementById('altura').value;
   const weight = document.getElementById('peso').value;
   const result = document.getElementById('resultado');
+  const resultName = document.getElementById('resultName');
+  const resultText = document.getElementById('resultText');
 
   if (name !== '' && height !== '' && weight !== '') {
     const imc = (weight / height ** 2).toFixed(2);
+    const minWeight = (18.5 * height ** 2).toFixed(2);
+    const maxWeight = (25 * height ** 2).toFixed(2);
 
-    let classification = '';
+    let classification;
 
     if (imc < 18.5) {
       classification = 'abaixo do peso.';
@@ -25,9 +29,16 @@ function calculateImc() {
       classification = 'com obesidade grau III.';
     }
 
-    result.textContent = `Olá, ${name}! Você está com ${weight}kg e ${height}m, seu IMC é ${imc}. Atualmente, você está ${classification}`;
+    resultName.textContent = `Olá, ${name}!`;
+    resultText.textContent = `Você está com ${weight}kg e ${height}m, seu  IMC é ${imc}.
+                              Atualmente, você está ${classification}`;
+    resultIdealWeigth.textContent = `Procure manter seu peso entre ${minWeight}kg e ${maxWeight}kg`;
   } else {
+    // Caso o usuario não preencha os input's limpar o resultName e resultText e no result informar o problema
     result.textContent = 'Para calcular o seu IMC , preencha todos os campos.';
+
+    resultName.textContent = '';
+    resultText.textContent = '';
   }
 }
 
